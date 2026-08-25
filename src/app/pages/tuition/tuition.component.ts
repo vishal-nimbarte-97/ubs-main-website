fd2b4a178073cb75b736b692675c9551b4304d19
Vishal Nimbarte <vishalnimbarte097@gmail.com>
2026-08-25 22:41:51 +0530

    minor changes

--- src/app/pages/tuition/tuition.component.ts
+++ src/app/pages/tuition/tuition.component.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
+
+interface TuitionRow {
+  programme: string;
+  mainCampus: string;
+  onlineCampus: string;
+  extension: string;
+}
 
 @Component({
   selector: 'app-tuition',
   standalone: true,
-  imports: [RouterLink],
+  imports: [CommonModule, RouterLink],
   templateUrl: './tuition.component.html',
   styleUrl: './tuition.component.scss',
 })
-export class TuitionComponent {}
+export class TuitionComponent {
+  readonly tuitionRows: TuitionRow[] = [
+    { programme: 'Residential programmes', mainCampus: 'To be updated', onlineCampus: 'Not applicable', extension: 'Not applicable' },
+    { programme: 'Non-residential programmes', mainCampus: 'Not applicable', onlineCampus: 'To be updated', extension: 'To be updated' },
+    { programme: 'Short-term courses', mainCampus: 'To be updated', onlineCampus: 'Contact UBS', extension: 'Contact UBS' },
+  ];
+}
